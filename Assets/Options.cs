07b9d93adfe62d6ee0866aa9e3f74a797df0072c
Anshi Mathur <anshimathur0325@gmail.com>
2026-08-31 20:57:14 -0500

    updated files

--- Assets/Options.cs
+++ Assets/Options.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
+
 public class Options : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Button m_YourFirstButton;
-    public Slider sli;
+    public Slider sli, sli2;
 
     public GameObject myPrefab;
     public Image panel;
+    public AudioMixer mixer;
     void Start()
     {
         m_YourFirstButton.onClick.AddListener(TaskOnClick);
         // color.onClick.AddListener(changeColor);
-        sli.value = AudioListener.volume;
+        // sli.value = BackgroundMusic;
+        // sli2.value = SoundEffects;
+        sli.value = 1;
+        sli2.value = 1;
         sli.onValueChanged.AddListener(changeSound);
+        sli2.onValueChanged.AddListener(changeSound2);
     }
     void TaskOnClick()
     {
@@ -28,7 +35,11 @@ public class Options : MonoBehaviour
     // }
     void changeSound(float s)
     {
-        AudioListener.volume = s;
+        mixer.SetFloat("BackgroundMusic", s);
+    }
+    void changeSound2(float s)
+    {
+        mixer.SetFloat("SoundEffects", s);
     }
     void Update()
     {
